e58d7a7a4c113354fc6d3baa2a8f531494a1d959
Aviator <dtrthuan@gmail.com>
2026-01-28 11:53:04 -0500

    Create Conveyor.java

---
+++ src/main/java/frc/robot/subsystems/Conveyor.java
@@ -0,0 +1,66 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.PersisMode;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.FeedbackSensor;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+
+  private final SparkFlex motor;
+  private final RelativeEncoder encoder;
+  private final SparkClosedLoopController pid;
+  
+  private double setRPM;
+  private double tolerance = 10;
+ 
+  /** Creates a new Intake. */
+  public Intake() {
+    motor = new SparkFlex(14, MotorType.kBrushless);
+    encoder = motor.getEncoder();
+    pid = motor.getClosedLoopController();
+    configureMotor();
+  }
+
+  public void configureMotor() {
+    SparkFlexConfig config = new SparkFlexConfig();
+
+    //Motor output
+    config.IdleMode(IdleMode.kCoast);
+    config.inverted(false);
+
+    //Current Limits
+    config.smartCurrentLimit(60);
+    config.secondaryCurrentLimit(70);
+  
+    //Encoder
+    double INTAKE_GEAR_RATIO = 1;
+    config.encoder.positionConversionFactor(1 / INTAKE_GEAR_RATIO);
+    config.encoder.velocityConversionFactor(1 / INTAKE_GEAR_RATIO);
+    
+    config.closedLoop
+      .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+      .pid(0.1, 0, 0);
+    motor.configure(config, ResetMode.kResetSafeParameters, PersisMode.kPersistParameters);
+
+  }
+  public void setSpeed(double rpm){
+    setRPM = rpm;
+    pid.setpoint(rpm, ControlType.kVelocity);
+  }
+  public boolean isAtSpeed(){
+    return Math.abs(encoder.getVelocity() - setRPM) <= tolerance;
+  }
+}
